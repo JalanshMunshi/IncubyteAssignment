bcdf426c18f0ae02679dc238cb46ea337e336cb9
JalanshMunshi <jalansh.munshi1366@gmail.com>
2020-05-24 14:30:37 +0530

    Added test case for unknown number of numbers.

--- IncubyteAssignment/src/StringCalculatorTests.java
+++ IncubyteAssignment/src/StringCalculatorTests.java
@@ -2,33 +2,54 @@ import org.junit.platform.commons.annotation.Testable;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
+import java.util.Random;
+
 import org.junit.Assert;
 
 import org.junit.Test;
+import org.junit.jupiter.api.BeforeAll;
 
 public class StringCalculatorTests {
 	
 	@Test
 	public void EmptyStringReturnsZero()
 	{
-		StringCalculator c = new StringCalculator();
-		assertEquals(0, c.Add(""));
+		StringCalculator calc = new StringCalculator();
+		assertEquals(0, calc.Add(""));
 	}
 	
 	@Test
-	public void StringWithSingleNumberReturnsTheNumberItself()
+	public void SingleNumberReturnsTheNumberItself()
 	{
-		StringCalculator c = new StringCalculator();
+		StringCalculator calc = new StringCalculator();
 		String num = "9";
-		assertEquals(9, c.Add(num));
+		assertEquals(9, calc.Add(num));
 	}
 	
 	@Test
-	public void StringWithTwoNumbersRetursTheirSum()
+	public void TwoNumbersRetursTheirSum()
 	{
-		StringCalculator c = new StringCalculator();
+		StringCalculator calc = new StringCalculator();
 		String num = "9,14";
-		assertEquals(23, c.Add(num));
+		assertEquals(23, calc.Add(num));
+	}
+	
+	@Test
+	public void UnknownNumbersReturnTheirValidSum()
+	{
+		StringCalculator calc = new StringCalculator();
+		Random r = new Random();
+		int totalNums = r.nextInt(20), expectedSum = 0;
+		String nums = "";
+		for(int i=0; i<totalNums; i++)
+		{
+			int t = r.nextInt(50);
+			nums += String.valueOf(t);
+			if(i<totalNums-1)
+				nums += ",";
+			expectedSum += t;
+		}
+		assertEquals(expectedSum, calc.Add(nums));
 	}
 
 }
