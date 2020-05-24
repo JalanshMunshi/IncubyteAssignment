9172542a1bffdde260c2b66c662eda032bb7c002
DELL <DELL@192.168.0.107>
2020-05-24 11:40:40 +0530

    Added logic for zero numbers.

--- IncubyteAssignment/src/StringCalculator.java
+++ IncubyteAssignment/src/StringCalculator.java
@@ -1,8 +1,11 @@
 
 public class StringCalculator {
-
-	public int Add(String string) {
+	
+//	Add function that takes up to 2 numbers
+	public int Add(String nums) {
 		
+		if(nums.length() == 0)
+			return 0;
 		return -1;
 	}
 
